4c5adea5f5f6aa9c1f5e7789345654271d0f8c32
Josue <jguille28@gmail.com>
2020-10-15 00:55:44 -0500

    Variable de entorno Mongodb2

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlConexion;
 if (process.env.NODE_ENV === 'dev') {
     urlConexion = 'mongodb://localhost:27017/cafe';
 } else {
-    urlConexion = MONGO_URI;
+    urlConexion = process.env.MONGO_URI;
 }
 
 process.env.URL_DB_CONEXION = urlConexion;
